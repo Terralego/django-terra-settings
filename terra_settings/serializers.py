2d5ffc7cd2feab31a38124ca6dda17596d7f35cf
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-30 17:05:15 +0100

    manage base layer tiles in API

--- terra_settings/serializers.py
+++ terra_settings/serializers.py
@@ -7,6 +7,12 @@ class BaseLayerTileSerializer(serializers.ModelSerializer):
         model = BaseLayerTile
         fields = ('url', )
 
+    def to_representation(self, instance):
+        return instance.url
+
+    def to_internal_value(self, data):
+        return data
+
 
 class MapBaseLayerSerializer(serializers.ModelSerializer):
     tiles = BaseLayerTileSerializer(many=True)
@@ -14,3 +20,27 @@ class MapBaseLayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = MapBaseLayer
         fields = "__all__"
+
+    def create(self, validated_data):
+        """ handle sub tile definition """
+        tiles = validated_data.pop('tiles')
+        instance = MapBaseLayer.objects.create(**validated_data)
+        for tile in tiles:
+            BaseLayerTile.objects.create(base_layer=instance, url=tile)
+        return instance
+
+    def update(self, instance, validated_data):
+        """ handle sub tile definition """
+        tiles = validated_data.pop('tiles')
+        for key, value in validated_data.items():
+            # update data for each instance validated value
+            setattr(instance, key, value)
+        # delete tiles not in putted data
+        instance.tiles.exclude(url__in=[tile for tile in tiles]).delete()
+        tile_instances = [
+            BaseLayerTile.objects.get_or_create(url=tile,
+                                                base_layer=instance)[0] for tile in tiles
+        ]
+        instance.tiles.set(tile_instances)
+        instance.save()
+        return instance
--- terra_settings/tests/test_views.py
+++ terra_settings/tests/test_views.py
@@ -7,7 +7,7 @@ from rest_framework.test import APITestCase
 
 class SettingsViewTestCase(APITestCase):
     def test_view(self):
-        response = self.client.get(reverse('terra_settings:settings'))
+        response = self.client.get(reverse('settings'))
         self.assertEqual(200, response.status_code)
         self.assertListEqual(
             ['jwt_delta', 'base_layers'],
@@ -35,13 +35,54 @@ class MapBaseLayerViewsSetTesCase(APITestCase):
         self.assertEqual(data['name'], 'OSM')
 
     def test_create(self):
-        pass
+        data = {
+            "tiles": [
+                "//test.org",
+            ],
+            "name": "test",
+            "order": 0,
+            "base_layer_type": "raster",
+            "min_zoom": 0,
+            "max_zoom": 20,
+            "tile_size": 256
+        }
+        response = self.client.post(reverse('baselayer-list'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = response.json()
+        self.assertTrue(data['id'])
+        self.assertEqual(data['tiles'][0], "//test.org")
 
     def test_partial_update(self):
-        pass
+        data = {
+            "tiles": [
+                "//test2.org",
+            ],
+        }
+        pk = MapBaseLayer.objects.get().pk
+        response = self.client.patch(reverse('baselayer-detail', args=(pk, )), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertEqual(data['tiles'][0], "//test2.org")
 
     def test_full_update(self):
-        pass
+        data = {
+            "tiles": [
+                "//test3.org",
+            ],
+            "name": "test",
+            "order": 0,
+            "base_layer_type": "raster",
+            "min_zoom": 0,
+            "max_zoom": 20,
+            "tile_size": 256
+        }
+        pk = MapBaseLayer.objects.get().pk
+        response = self.client.patch(reverse('baselayer-detail', args=(pk, )), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertEqual(data['tiles'][0], "//test3.org")
 
     def test_delete(self):
-        pass
+        pk = MapBaseLayer.objects.get().pk
+        response = self.client.delete(reverse('baselayer-detail', args=(pk, )))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
