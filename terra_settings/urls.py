2f06956e69588729756f7511b08de24bde74cd9d
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-30 16:13:07 +0100

    fix url prefix and formatting

--- terra_settings/urls.py
+++ terra_settings/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from terra_settings.views import SettingsView, BaseLayerViewSet
 
-app_name = 'terra_settings'
 
 router = SimpleRouter()
 router.register('baselayer', BaseLayerViewSet, basename='baselayer')
 
 urlpatterns = [
     path('settings/', SettingsView.as_view(), name='settings'),
-] + router.urls
+    path('', include(router.urls)),
+]
