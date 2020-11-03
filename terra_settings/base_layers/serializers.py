fa085a3290f356f135c85f374e8c66ad805d8470
J-E Castagnede <j.e.castagnede@gmail.com>
2020-11-03 11:12:23 +0100

    custom validation along mapbox base layer type

--- terra_settings/base_layers/serializers.py
+++ terra_settings/base_layers/serializers.py
@@ -1,5 +1,8 @@
+from django.utils.translation import gettext as _
 from mapbox_baselayer.models import MapBaseLayer, BaseLayerTile
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.reverse import reverse
 
 
 class BaseLayerTileSerializer(serializers.ModelSerializer):
@@ -16,16 +19,30 @@ class BaseLayerTileSerializer(serializers.ModelSerializer):
 
 class MapBaseLayerSerializer(serializers.ModelSerializer):
     tiles = BaseLayerTileSerializer(many=True)
-    tilejson_url = serializers.HyperlinkedIdentityField(view_name='baselayer-tilejson', read_only=True)
+    tilejson_url = serializers.SerializerMethodField()
 
-    class Meta:
-        model = MapBaseLayer
-        fields = (
-            'id', 'tiles', 'name', 'order', 'slug',
-            'base_layer_type', 'map_box_url', 'sprite',
-            'glyphs', 'min_zoom', 'max_zoom',
-            'tile_size', 'attribution', 'tiles', 'tilejson_url'
-        )
+    def get_tilejson_url(self, instance):
+        """
+        Provide generated tilejson or mapbox url along base layer type
+        """
+        if instance.base_layer_type != 'mapbox':
+            return reverse('baselayer-tilejson', args=(instance.pk, ),
+                           request=self.context.get('request'))
+        else:
+            return instance.map_box_url
+
+    def validate(self, attrs):
+        if attrs.get('base_layer_type') == 'mapbox':
+            if attrs.get('tiles'):
+                raise ValidationError(_("'tiles' should not be filled for mapbox base layers."))
+            if not attrs.get('map_box_url'):
+                raise ValidationError(_("'map_box_url' should be filled for mapbox base layers."))
+        else:
+            if not attrs.get('tiles'):
+                raise ValidationError(_("'tiles' should be filled for raster and vector base layers."))
+            if attrs.get('map_box_url'):
+                raise ValidationError(_("'map_box_url' should not be filled for raster and vector base layers."))
+        return attrs
 
     def create(self, validated_data):
         """ handle sub tile definition """
@@ -51,10 +68,28 @@ class MapBaseLayerSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
+    class Meta:
+        model = MapBaseLayer
+        fields = (
+            'id', 'tiles', 'name', 'order', 'slug',
+            'base_layer_type', 'map_box_url', 'sprite',
+            'glyphs', 'min_zoom', 'max_zoom',
+            'tile_size', 'attribution', 'tiles', 'tilejson_url'
+        )
+
 
 class PublicMapBaseLayerSerializer(serializers.ModelSerializer):
     """ Serializer used to provide quick select list in default public settings API endpoint """
-    tilejson_url = serializers.HyperlinkedIdentityField(view_name='baselayer-tilejson', read_only=True)
+    tilejson_url = serializers.SerializerMethodField()
+
+    def get_tilejson_url(self, instance):
+        """
+        Provide generated tilejson or mapbox url along base layer type
+        """
+        if instance.base_layer_type != 'mapbox':
+            return reverse('baselayer-tilejson', args=(instance.pk, ))
+        else:
+            return instance.map_box_url
 
     class Meta:
         model = MapBaseLayer
